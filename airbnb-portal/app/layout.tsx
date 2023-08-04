0aeedd42c72cc10dace6374bf55773b32a8b5b5c
ThanhNT26 <thanhnt26@vng.com.vn>
2023-08-04 16:20:55 +0700

    feat: register modal UI

--- airbnb-portal/app/layout.tsx
+++ airbnb-portal/app/layout.tsx
@@ -2,7 +2,9 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import type { Metadata } from 'next'
 import Navbar from './components/navbar/Navbar'
+import RegisterModal from './components/modals/RegisterModal'
 import ClientOnly from './components/ClientOnly'
+import ToasterProvider from './providers/ToasterProvider'
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -22,6 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal/>
           <Navbar/>
         </ClientOnly>
         {children}
