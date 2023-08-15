8e7832abb72956937183972b777844d5fdc8528a
ThanhNT26 <thanhnt26@vng.com.vn>
2023-08-15 17:08:59 +0700

    feat: login, logout functionality

--- airbnb-portal/app/components/navbar/UserMenu.tsx
+++ airbnb-portal/app/components/navbar/UserMenu.tsx
@@ -5,32 +5,34 @@ import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
 import useRegisterModal from '@/app/hooks/useRegisterModal'
+import useLoginModal from '@/app/hooks/useLoginModal'
+import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
-const UserMenu = () => {
+interface UserMenuProps {
+  currentUser?: SafeUser | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  currentUser
+}) => {
   const registerModal = useRegisterModal()
+  const loginModal = useLoginModal()
+
   const [isOpen, setIsOpen] = React.useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((prev) => !prev);
   }, []);
 
+  console.log('currentUser: ', currentUser);
+
   return (
     <div className='relative'>
       <div className='flex flex-row items-center gap-3'>
         <div
           onClick={() => {}}
-          className='
-            hidden
-            md:block
-            text-sm
-            font-semibold
-            py-3
-            px-4
-            rounded-full
-            hover:bg-neutral-100
-            transition
-            cursor-pointer
-          '
+          className='hidden px-4 py-3 text-sm font-semibold transition rounded-full cursor-pointer md:block hover:bg-neutral-100'
         >
           Airbnb your home
         </div>
@@ -82,16 +84,49 @@ const UserMenu = () => {
             '
           >
             <div className='flex flex-col cursor-pointer'>
-              <>
-                <MenuItem
-                  onClick={() => {}}
-                  label='Login'
-                />
-                <MenuItem
-                  onClick={registerModal.onOpen}
-                  label='Sign up'
-                />
-              </>
+              {
+                currentUser ? (
+                  <>
+                    <MenuItem
+                      onClick={() => {}}
+                      label='My trips'
+                    />
+                    <MenuItem
+                      onClick={() => {}}
+                      label='My favorites'
+                    />
+                    <MenuItem
+                      onClick={() => {}}
+                      label='My reservations'
+                    />
+                    <MenuItem
+                      onClick={() => {}}
+                      label='My properties'
+                    />
+                    <MenuItem
+                      onClick={() => {}}
+                      label='Airbnb my home'
+                    />
+                    <MenuItem
+                      onClick={() => signOut()}
+                      label='Logout'
+                    />
+                  </>
+                ) : (
+                  <>
+                    <MenuItem
+                      onClick={loginModal.onOpen}
+                      label='Login'
+                    />
+                    <MenuItem
+                      onClick={registerModal.onOpen}
+                      label='Sign up'
+                    />
+                  </>
+                )
+              }
+
+
             </div>
           </div>
         )
--- airbnb-portal/app/layout.tsx
+++ airbnb-portal/app/layout.tsx
@@ -3,8 +3,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Navbar from './components/navbar/Navbar'
 import RegisterModal from './components/modals/RegisterModal'
+import LoginModal from './components/modals/LoginModal'
 import ClientOnly from './components/ClientOnly'
 import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata: Metadata = {
   title: 'Airbnb',
@@ -15,18 +17,20 @@ const font = Nunito({
   subsets: ['latin'],
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal/>
-          <Navbar/>
+          <LoginModal/>
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
       </body>
